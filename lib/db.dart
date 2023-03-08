820f7d9183ca7a90795a0e952ac22d01fd923561
Yash Jani <yash.jani@prodcrew@gmail.com>
2023-03-08 16:54:12 +0530

    Syntax error solved

--- lib/db.dart
+++ lib/db.dart
@@ -17,7 +17,7 @@ class MyDb {
                           fname varchar(255) not null,
                           mno int not null,
                           lname varchar(255) not null,
-                          email varchar(255) not null,
+                          email varchar(255) not null
                       );
                       
                       //create more table here
